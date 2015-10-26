dc8180265e4c06372b8b641843ba9488a9573bf0
yular <1062969706@qq.com>
2015-10-26 00:09:41 -0700

    AngularJS template update

--- AngularJs/js/plugin.js
+++ AngularJs/js/plugin.js
@@ -1,4 +1,5 @@
 (function() {
 
+ $("#inputArea").linedtextarea();
 
-});
+}());
--- AngularJs/js/templatehomeCtrl.js
+++ AngularJs/js/templatehomeCtrl.js
@@ -1,14 +1,14 @@
 templateVar
-	.config(['$httpProvider', function($httpProvider) {
-        	delete $httpProvider.defaults.headers.common['X-Requested-With'];
-       		$httpProvider.defaults.useXDomain = true;
-    	}])
-	.controller('templatehomeCtrl', ['$scope', '$http', function($scope, $http) {
-		var init = function () {
+ .config(['$httpProvider', function($httpProvider) {
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+       	$httpProvider.defaults.useXDomain = true;
+  }])
+  .controller('templatehomeCtrl', ['$scope', '$http', function($scope, $http) {
+	var init = function () {
 
-		}
+	}
 
-		init();
+	init();
 
-	}]);
+   }]);
 
